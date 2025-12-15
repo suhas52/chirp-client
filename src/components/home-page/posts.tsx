e1f17d5cffd14ab86a63da87009c455ca1f54850
suhas52 <suhasedu991@gmail.com>
2025-12-15 11:14:13 +0530

    like and retweet svgs in the posts component

--- src/components/home-page/posts.tsx
+++ src/components/home-page/posts.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Item, ItemContent, ItemDescription, ItemGroup, ItemTitle } from "../ui/item";
+import { Item, ItemActions, ItemContent, ItemDescription, ItemGroup, ItemTitle } from "../ui/item";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
@@ -75,7 +75,14 @@ export default function Posts() {
                         </div>
                         <p>{post.content}</p>
                     </ItemContent>
-
+                    <ItemActions>
+                        <svg width="20px" height="20px" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M725.333333 192c-89.6 0-168.533333 44.8-213.333333 115.2C467.2 236.8 388.266667 192 298.666667 192 157.866667 192 42.666667 307.2 42.666667 448c0 253.866667 469.333333 512 469.333333 512s469.333333-256 469.333333-512c0-140.8-115.2-256-256-256z" /></svg>
+                        <svg fill="#000000" width="20px" height="20px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <title>retweet</title>
+                            <path d="M8.013 22.033v-7.972h3.932l-5.902-6.892-6.026 6.893h3.947v11.896h17.468l-3.923-3.924h-9.496zM28.036 19.001v-11.958h-17.531l3.986 3.985h9.496v7.973h-3.932l5.901 6.893 6.026-6.893h-3.946z"></path>
+                        </svg>
+                    </ItemActions>
                 </Item>
             ))
         )}
