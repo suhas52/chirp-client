dae087c3f8a0898b723af5ee895ccce15ec760d1
suhas52 <suhasedu991@gmail.com>
2025-12-21 13:10:43 +0530

    avatar and username in the details card

--- src/components/user-profile/user-details.tsx
+++ src/components/user-profile/user-details.tsx
@@ -1,3 +1,5 @@
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 interface UserObject {
     id: string;
@@ -13,7 +15,20 @@ export default function UserDetails({ user }: { user: UserObject }) {
     console.log(user)
 
     return <div>
-
+        <Card>
+            <CardHeader>
+                <CardTitle>User Details</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="flex items-center gap-5">
+                    <Avatar >
+                        <AvatarImage className="rounded" src={user.avatarUrl} />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <p>{user.username}</p>
+                </div>
+            </CardContent>
+        </Card>
     </div>
 }
 
