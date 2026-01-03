13ffb7e934f1b58ef3faccfff66b2cd4b3c01b17
suhas52 <suhasedu991@gmail.com>
2026-01-03 15:30:46 +0530

    temporarily allow all hosts in vite

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   server: {
-	allowedHosts: ['chirp-infra-monorepo-react-1']
+	allowedHosts: true
   }
   },
 })
